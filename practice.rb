710dfc3f91ddbc69470a383464ec7ad76b506b28
ShinpeiH <dctechsf@gmail.com>
2023-06-14 12:41:35 -0700

    working on dividing_numbers

---
+++ practice.rb
@@ -0,0 +1,4 @@
+def valid_number?(number_string)
+  number_string.to_i.to_s == number_string
+end
+
--- user_input/dividing_numbers.rb
+++ user_input/dividing_numbers.rb
@@ -12,10 +12,13 @@ loop do
   puts '>> Please enter the denominator:'
   denominator = gets.chomp.to_i
   
-  break if valid_number?(numerator) && valid_number?(denominator)
   if denominator == 0
-    puts '>>Invalid input. A denominator of 0 is not allowed.'
+    puts ">> Invalid input. A denominator of 0 is not allowed."
+    next
   end
+  
+  break if valid_number?(numerator) && valid_number?(denominator)
+  
   puts ">> Invalid input. Only integers are allowed."
 end
 
